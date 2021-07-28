d0154e03f2edf34e3135c58e6879dc1f301129d6
freakmoister <freakmoister@gmail.com>
2021-07-29 09:16:53 +1000

    Update server.tf

--- training/server.tf
+++ training/server.tf
@@ -1,4 +1,12 @@
 resource "intersight_server_profile" "server1" {
-  data.intersight_organization_organization.organization_moid.moid
+  name = "Training_qiayu"
+  action = "No-op"
+  tags {
+    key = "server"
+    value = "demo"
+  }
+  organization {
+    object_type = "organization.Organization"
+    moid = data.intersight_organization_organization.organization_moid.moid
+  }
 }
-
